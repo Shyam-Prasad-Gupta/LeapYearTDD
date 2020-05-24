8e6c382d969631aa277e5deab40e6fccd9cbe0e3
Shyam P G <shyampra5380@gmail.com>
2020-05-24 05:50:46 +0530

    added the tests for 2nd and 3rd acceptance criteria

--- src/main/test/com/shyam/leapyear/test/TestLeapYear.java
+++ src/main/test/com/shyam/leapyear/test/TestLeapYear.java
@@ -14,6 +14,19 @@ class TestLeapYear {
 		//If all years are divisible by 400.
 		//Test with input 2000
 		Assertions.assertEquals(Boolean.TRUE, LeapYear.checkForLeapYear(2000));
+		
+		//If all years divisible by 100 but not by 400. 
+		//Test with inputs 1700, 1800, 2100, and 1900
+		Assertions.assertEquals(Boolean.TRUE, LeapYear.checkForLeapYear(1700));
+		Assertions.assertEquals(Boolean.TRUE, LeapYear.checkForLeapYear(1800));
+		Assertions.assertEquals(Boolean.TRUE, LeapYear.checkForLeapYear(1900));
+		Assertions.assertEquals(Boolean.TRUE, LeapYear.checkForLeapYear(2100));
+		
+		//If year is divisible by 4 but not by 100. 
+		//Test with inputs 2008, 2012, and 2016
+		Assertions.assertEquals(Boolean.TRUE, LeapYear.checkForLeapYear(2008));
+		Assertions.assertEquals(Boolean.TRUE, LeapYear.checkForLeapYear(2012));
+		Assertions.assertEquals(Boolean.TRUE, LeapYear.checkForLeapYear(2016));
 				
 	}
 
